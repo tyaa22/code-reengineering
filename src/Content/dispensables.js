1bf5c28df9e175f41896db83ea1a44196274bad6
cyintia <meteorite01@outlook.com>
2024-04-13 23:09:46 +0700

    updates

--- src/Content/dispensables.js
+++ src/Content/dispensables.js
@@ -1,4 +1,6 @@
-{
+const dispen = {
     "title": "Dispensables",
     "txt": "Hal - hal tidak penting yang dapat menurunkan kualitas kode yang dihasilkan. Refactoring dilakukan untuk membuat kode menjadi lebih rapi, efisien, dan mudah untuk dimengerti."
 }
+
+export default dispen;
